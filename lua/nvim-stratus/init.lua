92b0f9a33d2942c7c4c64211986838c5ba9886b3
sam3llis <samwilliamellis@gmail.com>
2022-03-31 18:07:10 +0100

    chore: remove components and catppuccin to personal config
    I've moved my personal selection of components to my Neovim .config and
    slightly changed the setup call to nvim-stratus. We now require a
    'components' variable as an input to the .setup() function, which uses
    the same options as that found in the .attach_component() function.

--- lua/nvim-stratus/init.lua
+++ lua/nvim-stratus/init.lua
@@ -1,4 +1,8 @@
+-- TODO: add 'format' to 'attach_components', e.g.,
+--   format = [block (current/default), strata (continous without spaces)].
 -- TODO: Create components for word count, spell=on, etc.
+-- TODO: Add '?change_on_colour? = true' option for 'attach_component' objects,
+--   which changes background of components on mode change.
 
 -- TODO: Check whether we actually require Neovim 0.7.0.
 if vim.fn.has('nvim-0.7.0') ~= 1 then
@@ -9,96 +13,11 @@ end
 
 local M = {}
 
-local utils = require('nvim-stratus.utils')
-local core = require('nvim-stratus.core')
-local separators = require('nvim-stratus.ui.separators')
-local cp = require('catppuccin.core.color_palette') -- TODO: Move this outside of the nvim-stratus plugin.
-local builtins = require('nvim-stratus.builtins')
-
-M.setup = function ()
+M.setup = function (components)
   vim.opt.statusline = ''
-
-  core.attach_component({
-    operation = function ()
-      return require('nvim-stratus.builtins').current_mode()
-    end,
-    style = {
-      fg = cp.black2,
-      bg = cp.teal,
-      gui = 'bold',
-    },
-    separator = separators.arrow,
-    position = 'left',
-  })
-
-  -- if vim.b.gitsigns_head ~= nil then
-    core.attach_component({
-      operation = function ()
-        return require('nvim-stratus.builtins').git_branch()
-      end,
-      style = {
-        fg = cp.black2,
-        bg = cp.gray2,
-        gui = 'bold',
-      },
-      separator = separators.arrow,
-    })
-  -- end
-
-  core.attach_component({
-    operation = function ()
-      return require('nvim-stratus.builtins').file_directory()
-    end,
-    style = {
-      fg = cp.black2,
-      bg = cp.gray2,
-      gui = 'bold',
-    },
-    separator = separators.arrow,
-  })
-
-  core.attach_component({
-    operation = function ()
-      return require('nvim-stratus.builtins').filename()
-    end,
-    style = {
-      fg = cp.black2,
-      bg = cp.gray2,
-      gui = 'bold',
-    },
-    separator = separators.arrow,
-  })
-
-  -- -- FIXME: moving around diagnostics using [g or ]g causes modification glitch in separators.
-  -- -- TODO: Disable on no LSP detected on buffer.
-  -- core.attach_component({
-  --   operation = "require('nvim-stratus.builtins').get_lsp_diagnostic()",
-  --   style = {
-  --     fg = cp.black2,
-  --     bg = cp.flamingo,
-  --     gui = 'bold',
-  --
-  --   },
-  --   separator = separators.arrow,
-  --   -- detach_on = vim.bo.modified == false,
-  --   position = 'left',
-  -- })
-
-  core.attach_component({
-    operation = function ()
-      return require('nvim-stratus.builtins').modified()
-    end,
-    style = {
-      fg = cp.black2,
-      bg = cp.yellow,
-      gui = 'bold',
-    },
-    separator = separators.arrow,
-    detach_on = vim.bo.modified == false,
-    position = 'left',
-    -- update_on = {'BufModifiedSet'},
-  })
-
+  for _, component in pairs(components) do
+    require('nvim-stratus.core').attach_component(component)
+  end
 end
 
 M.compile = function (components)
