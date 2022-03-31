bfd45877de256b68a1b45d2e041c875c6fc90909
sam3llis <samwilliamellis@gmail.com>
2022-03-31 18:14:43 +0100

    chore: remove duplicate table

--- lua/nvim-stratus/ui/modes.lua
+++ lua/nvim-stratus/ui/modes.lua
@@ -1,32 +1,4 @@
 -- -- FIXME: Modes 'V.Block' and 'S.Block' do not work.
--- local M = setmetatable({
---   ['n']  = 'Normal',
---   ['no'] = 'N·Pending',
---   ['v']  = 'Visual',
---   ['V']  = 'V·Line',
---   [''] = 'V·Block',
---   ['s']  = 'Select',
---   ['S']  = 'S·Line',
---   [''] = 'S·Block',
---   ['i']  = 'Insert',
---   ['ic'] = 'Insert',
---   ['R']  = 'Replace',
---   ['Rv'] = 'V·Replace',
---   ['c']  = 'Command',
---   ['cv'] = 'Vim·Ex',
---   ['ce'] = 'Ex',
---   ['r']  = 'Prompt',
---   ['rm'] = 'More',
---   ['r?'] = 'Confirm',
---   ['!']  = 'Shell',
---   ['t']  = 'Terminal',
--- }, {
---   __index = function()
---     return 'Unknown'
---   end
--- })
-
--- return M
 local M = {
   ['n'] = 'NORMAL',
   ['no'] = 'OP',
